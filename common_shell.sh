cdfe60e36fd676c1009e591b374f18fcfb7850ec
cuizhenlei <318213421@qq.com>
2017-08-01 20:00:23 +0800

    第一次提交
    首次提交，添加tcpstat命令

---
+++ common_shell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#tcpnet
+#show tcp state
+
+#state			count
+#FIN_WAIT_1		4
+#FIN_WAIT_2		15
+#LAST_ACK		4
+#CLOSE_WAIT		15
+#TIME_WAIT		2
+#ESTABLISHED	6
+
+netstat -f inet -p tcp -n | awk '{if(NR < 3){next} state[$NF]++}END{printf("state\t\tcount\n");for (item in state) printf("%s\t%d\n", item, state[item])}'
